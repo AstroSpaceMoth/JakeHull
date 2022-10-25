fb15585cb7d113a969fa060c332d882629e45ddb
Jake <astrospacemoth@gmail.com>
2022-10-25 19:47:48 -0400

    Style Change

--- trailer.js
+++
@@ -1,8 +0,0 @@
-const trailer = document.getElementById("trailer");
-
-window.onmousemove = (e) => {
-  const x = e.clientX - trailer.offsetWidth / 2,
-        y = e.clientY - trailer.offsetHeight / 2;
-  
-  trailer.style.transform = `translate(${x}px, ${y}px)`;
-}
